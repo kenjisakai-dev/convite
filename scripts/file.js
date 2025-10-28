761cb8749601291f0244fc94f8b2dbc72450e7fa
Kenji <kenjisakai80@gmail.com>
2025-10-28 20:30:07 -0300

    create inputs theme style and upload cover photo

---
+++ scripts/file.js
@@ -0,0 +1,11 @@
+const inputFile = document.querySelector(".file-wrapper input[type='file']");
+const labelFile = document.querySelector(".file-wrapper > label");
+
+inputFile.addEventListener("change", function () {
+  if (inputFile.files.length === 1) {
+    labelFile.style.color = "var(--text-body)";
+
+    labelFile.textContent =
+      inputFile?.files[0]?.name ?? "Nenhum arquivo selecionado";
+  }
+});
